5be8f846f82c10348cfdff1c367cd0789de9c0d9
Shipradwivedi1212 <63915485+Shipradwivedi1212@users.noreply.github.com>
2024-11-29 19:12:26 +0530

    Add files via upload

---
+++ Addition.js
@@ -0,0 +1,29 @@
+function addition(str){
+try{
+    var total = 0;
+    var ng = "";
+    var x;
+   for(var i=0;i< str.length;i++){
+    x = str[i];
+    if(parseInt(x)){
+        if(str[i-1]=="-"){
+            x= "-"+x;
+        }
+        if(parseInt(x)>0){
+            total = total + parseInt(x);
+        }
+        else{
+            ng = ng + " "+x.slice(1);
+        }
+        }
+   }
+   if(ng){
+    throw (`negative numbers not allowed ${ng}`)
+   }
+   return total;
+}
+catch(e){
+  console.log(e);
+}
+}
+addition("//;\n1;2");
